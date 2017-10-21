b292ead7b95fa912631ff1323e32c12a5572584c
Sina Siddiqi <sinas@andrew.cmu.edu>
2017-10-21 00:30:23 -0400

    Built item model but not tested it yet

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,42 @@ class Item < ActiveRecord::Base
   has_many :order_items
   has_many :orders, through: :order_items
   has_many :color_items
+  has_many :colors, through: :color_items
+
+  # Scopes
+  scope :alphabetical, -> { order(:name) }
+  scope :active,       -> { where(active: true) }
+  scope :inactive,     -> { where(active: false) }
+  # scope :for_color, ->(color_id) { joins(:color).where(color: color_id) }
+  # scope :for_condition, ->(condition_id) { joins(:condition).where(condition: condition_id) }
+  # scope :for_size, ->(size_id) { joins(:size).where(size: size_id) }
+  # scope :available, -> { joins(:order_items).where('order_items.returned = ?', false) }
+
+  # Validations
+  validates_presence_of :name, :renting_price, :selling_price
+  validates_numericality_of :renting_price, :selling_price, greater_than_or_equal_to: 0
+  validates_numericality_of :avg_rating, greater_than_or_equal_to: 0, less_than_or_equal_to: 5
+
+  # Callbacks
+  before_destroy :is_destroyable?
+  after_destroy :remove_unshipped_and_unpaid_order_items
+  after_rollback :convert_to_inactive
+
+  # Methods
+  private
+  def is_destroyable?
+    @destroyable = self.order_items.empty?
+  end
+  
+  def convert_to_inactive
+    if !@destroyable.nil? && @destroyable == false
+      make_inactive
+    end
+    @destroyable = nil
+  end
+
+  def make_inactive
+    self.update_attribute(:active, false)
+  end
+
 end
--- db/migrate/20171019230711_create_statuses.rb
+++ db/migrate/20171019230711_create_statuses.rb
@@ -2,6 +2,7 @@ class CreateStatuses < ActiveRecord::Migration
   def change
     create_table :statuses do |t|
       t.string :name
+      t.boolean :active
 
       t.timestamps null: false
     end
